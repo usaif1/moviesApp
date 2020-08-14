86531c58aeb7e9d97b3e9dfe116bf090afddabcb
usaif1 <usaif1311@gmail.com>
2020-08-14 22:17:59 +0530

    added carousel on homepage

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,14 +1,38 @@
 import React from "react"
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+import { Carousel } from "react-responsive-carousel"
+
+import alita from "../../assets/posters/alita.jpg"
+import avengersEndgame from "../../assets/posters/avengers-endgame.jpg"
+import avengersInfinityWar from "../../assets/posters/avengers-infinity-war.jpg"
+import interstellar from "../../assets/posters/interstellar.jpg"
+import joker from "../../assets/posters/joker.jpg"
+import justiceLeague from "../../assets/posters/justice-league.jpg"
 
 const Home = () => {
 	return (
 		<div className="container">
-			<div className="text-left">
-				<h1>Welcome to Movie Finder App</h1>
-			</div>
-			<div className="mt-5 text-left">
-				<h5>Here you can get all the information about the latest movies</h5>
-			</div>
+			<h1>All your favourite movies in one place</h1>
+			<Carousel style={{ height: "50%", width: "100%" }}>
+				<div>
+					<img src={alita} alt="first-pic" />
+				</div>
+				<div>
+					<img src={avengersEndgame} alt="second-pic" />
+				</div>
+				<div>
+					<img src={avengersInfinityWar} alt="first-pic" />
+				</div>
+				<div>
+					<img src={interstellar} alt="second-pic" />
+				</div>
+				<div>
+					<img src={joker} alt="first-pic" />
+				</div>
+				<div>
+					<img src={justiceLeague} alt="second-pic" />
+				</div>
+			</Carousel>
 		</div>
 	)
 }
